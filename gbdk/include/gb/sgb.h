bf46bce1242f751bb049057080d45dfc7c7c25f0
cvs2git <git@basxto.de>
2000-02-28 01:42:36 +0000

    This commit was manufactured by cvs2git to create tag 'gbdk-291'.

--- gbdk/include/gb/sgb.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SGB_H
-#define _SGB_H
-
-UBYTE
-sgb_check(void);
-/* Return a non-null value if running on Super GameBoy */
-
-#endif /* _SGB_H */
--- gbdk/lib-gb/tolower.c
+++
@@ -1,6 +0,0 @@
-#include <ctype.h>
-
-BYTE tolower(char c)
-{
-  return ((c >= 'A' && c <= 'Z') ? c + 32: c);
-}
