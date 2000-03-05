ee1d4d57e1cc03cd651e2ebbaf1879d05c684b13
Michael Hope <michaelh@juju.net.nz>
2000-03-05 22:11:32 +0000

    Pre 2.92. * colorbar works * Fixed volatile regs bug * dscan and gb-dtmf segfault.

--- gbdk-lib/libc/gb/crt0.s
+++ gbdk-lib/libc/gb/crt0.s
@@ -245,6 +245,7 @@
 	;; Call the main function
 	CALL	_main
 99$:
+	HALT
 	JR	99$		; Wait forever
 
 	.org	.MODE_TABLE
@@ -261,6 +262,7 @@
 	;; Constant data used to init _DATA
 	.area	_GSINIT
 	.area	_GSINITTAIL
+	.area	_GSFINAL
 	;; Initialised in ram data
 	.area	_DATA
 	;; Uninitialised ram data
